959af68cc6155e1943f0bb8ca049a14fc8a9a316
ErlindaAR <aprityranirischalidaherlinda@gmail.com>
2021-09-29 20:33:17 +0700

    Menambahkan Framework

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php 
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Erlinda Aprityrani Rischalidah
+            Saya tinggal di daerah Tasikmalaya
+            No.HP 085871571168";
+    }
+}
